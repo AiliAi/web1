8e22f2290b5f49169f002374c2bf643120ba4c20
AiliAi <49479422+AiliAi@users.noreply.github.com>
2020-11-28 22:41:02 +0200

    big change

--- app.js
+++ app.js
@@ -1,27 +1,62 @@
 'use strict';
 const express = require('express');
 const cors = require('cors');
+const fs      = require('fs');
+const https = require('https');
 const bodyParser = require('body-parser');
 const catRoute = require('./routes/catRoute');
 const userRoute = require('./routes/userRoute');
 const passport = require('./utils/pass.js');
 const authRoute = require('./routes/authRoute');
 const app = express();
-const port = 3000;
+
+app.enable('trust proxy');
 
 app.use(cors());
-app.use(express.static('uploads'));
-app.use(express.static('public'));
-app.use('/thumbnails', express.static('thumbnails'));
-
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}));
 // parse application/json
 app.use(bodyParser.json());
 
+
+// if production redirect to https
+if (process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.secure) {
+      // request was via https, so do no special handling
+      next();
+    } else {
+      // if express app run under proxy with sub path URL
+      // e.g. http://www.myserver.com/app/
+      // then, in your .env, set PROXY_PASS=/app
+      // Adapt to your proxy settings!
+      const proxypath = process.env.PROXY_PASS || '';
+      // request was via http, so redirect to https
+      console.log(`https://${req.headers.host}${proxypath}${req.url}`);
+      res.redirect(301, `https://${req.headers.host}${proxypath}${req.url}`);
+    }
+  });
+}
+
+app.use(express.static('public'));
+app.use(express.static('uploads'));
+app.use('/thumbnails', express.static('thumbnails'));
+
 app.use('/auth', authRoute);
 app.use('/cat', passport.authenticate('jwt', {session: false}), catRoute);
 app.use('/user', passport.authenticate('jwt', {session: false}), userRoute);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// http
+app.listen(3000, () => console.log(`HTTP on port ${3000}!`));
 
+// if production, add https, with this if no need to install certs locally
+if (process.env.NODE_ENV === 'production') {
+  const sslkey = fs.readFileSync('/etc/pki/tls/private/ca.key');
+  const sslcert = fs.readFileSync('/etc/pki/tls/certs/ca.crt');
+  const options = {
+    key: sslkey,
+    cert: sslcert
+  };
+  https.createServer(options, app).listen(8000,
+      () => console.log(`HTTPS on port ${8000}!`)); //https traffic
+}
